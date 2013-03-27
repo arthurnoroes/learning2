32a89257453c5ceba694b24e0f25c6ee59afd22f
Arthur Noroes <arthurnoroes@gmail.com>
2013-03-27 20:34:50 -0300

    specifying in 2nd window

--- src/GUI.java
+++ src/GUI.java
@@ -1,6 +1,6 @@
 
 Main Window: " Welcome to this software"
 
-2nd Window: "Insert your problem"
+2nd Window: "Insert your problem and specify accuracy level"
 
 3rd Window: "Solution to your problem here"
