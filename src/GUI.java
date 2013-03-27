d40e1bb89500c3e46a8a471f6d70783796804c6d
Arthur Noroes <arthurnoroes@gmail.com>
2013-03-27 18:58:55 -0300

    specify problem input in 2nd window

--- src/GUI.java
+++ src/GUI.java
@@ -1,6 +1,6 @@
 
 Main Window: " Welcome to this software"
 
-2nd Window: "Insert your problem"
+2nd Window: "Insert your problem. Problems should be in the form x2+x3+x3<=10"
 
 3rd Window: "Solution to your problem here"
