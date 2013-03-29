3fe9d9435374bd3146fbe99ad20a4ec67ebc290f
Arthur Noroes <arthurnoroes@gmail.com>
2013-03-29 08:35:48 -0300

    Adding 5th window

--- src/GUI.java
+++ src/GUI.java
@@ -6,3 +6,5 @@ Main Window: " Welcome to this software"
 2nd Window: "Insert your problem and specify accuracy level.Problems should be in the form x2+x3+x3<=10"
 
 3rd Window: "Solution to your problem here"
+
+5th Window: "Statistics of your problem here"
