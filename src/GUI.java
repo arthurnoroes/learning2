25b1f77638af4b07adcc3b6b13e255a400ca129d
Arthur Noroes <arthurnoroes@gmail.com>
2013-03-29 08:29:59 -0300

    edit GUI

--- src/GUI.java
+++ src/GUI.java
@@ -1,4 +1,6 @@
 
+We should start working on the GUI now
+
 Main Window: " Welcome to this software"
 
 2nd Window: "Insert your problem and specify accuracy level.Problems should be in the form x2+x3+x3<=10"
