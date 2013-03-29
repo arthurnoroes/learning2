22dbb7ffe61d4b3f7e18c4c1294ea8e741cb9dd3
arthurnoroes <arthurnoroes@gmail.com>
2013-03-29 08:34:10 -0300

    add 4th Window

--- src/GUI.java
+++ src/GUI.java
@@ -1,8 +1,12 @@
 
 We should start working on the GUI now
 
+
+
 Main Window: " Welcome to this software"
 
 2nd Window: "Insert your problem and specify accuracy level.Problems should be in the form x2+x3+x3<=10"
 
 3rd Window: "Solution to your problem here"
+
+4th Window: "Graphical solution to your problem here"
